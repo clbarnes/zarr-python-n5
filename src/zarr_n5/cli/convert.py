f40ab4c91c8ca23e68dba22dd14743fa55b876de
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-06-17 14:43:56 +0100

    Minor conversion CLI changes

--- src/zarr_n5/cli/convert.py
+++ src/zarr_n5/cli/convert.py
@@ -11,13 +11,23 @@ def main(raw_args=None):
     parser = ArgumentParser("n5tozarr")
     parser.add_argument("url", help="URL to Zarr store, using fsspec format")
     parser.add_argument(
-        "path", help="paths within the Zarr store to process", nargs="?"
+        "path",
+        help="path within the Zarr store to process; default store root",
+        nargs="?",
     )
     parser.add_argument(
-        "-t", "--tasks", type=int, default=DEFAULT_TASKS, help="asynchronous task count"
+        "-t",
+        "--tasks",
+        type=int,
+        default=DEFAULT_TASKS,
+        help=f"asynchronous task count; default {DEFAULT_TASKS}",
     )
     parser.add_argument(
-        "-d", "--max-depth", type=int, help="how far to recurse; default no maximum"
+        "-d",
+        "--max-depth",
+        type=int,
+        default=-1,
+        help="how far to recurse; default no maximum",
     )
     parser.add_argument(
         "-I",
@@ -25,9 +35,20 @@ def main(raw_args=None):
         action="store_true",
         help="do not infer N5 groups from empty directories/ prefixes",
     )
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="overwrite existing valid zarr.json files",
+    )
 
     args = parser.parse_args(raw_args)
     fut = convert_hierarchy(
-        args.url, args.path or "", not args.no_infer_groups, args.max_depth, args.tasks
+        args.url,
+        args.path or "",
+        not args.no_infer_groups,
+        args.max_depth,
+        args.tasks,
+        args.overwrite,
     )
     asyncio.run(fut)
--- src/zarr_n5/convert.py
+++ src/zarr_n5/convert.py
@@ -34,35 +34,76 @@ class N5ToZarr:
         self.queue: asyncio.Queue[AsyncArray | AsyncGroup | Finished] = asyncio.Queue()
 
     async def convert_hierarchy(
-        self, path: str = "", max_depth: int | None = -1, n_tasks=10
+        self,
+        path: str = "",
+        max_depth: int = -1,
+        n_tasks=10,
+        overwrite_zarr_json: bool = False,
     ):
-        member = await zarr_async.open(store=self.n5_store, path=path)
-        total = await self.convert_member(member)
-        if total == 0:
-            return total
-        if isinstance(member, AsyncArray):
-            return total
-        if max_depth is not None and max_depth >= 0:
-            return total
+        """Convert an N5 hierarchy into zarr.
 
-        new_depth = None if max_depth is None else max_depth - 1
-        tasks = [self._spawn_worker() for _ in range(n_tasks)]
-        total = 0
-        async for _, child in member.members(new_depth):
-            await self.queue.put(child)
-            total += 1
-        logger.info("Enqueued %s nodes", total)
+        No existing objects are altered and no chunk data is copied;
+        a zarr.json is simply added to every N5 node.
+
+        Parameters
+        ----------
+        path
+            The path of the node to start at.
+        max_depth
+            <0: find all descendant nodes (default).
+            0: only convert the root.
+            n: convert descendants down to `n` levels (immediate children are 1, grandchildren are 2 etc.).
+        n_tasks
+            Number of async tasks started, by default 10
+        overwrite_zarr_json
+            Whether to overwrite existing zarr.json objects, by default False.
+            True saves having to check for (valid) zarr.json objects and so may be faster.
+
+        Returns
+        -------
+        count
+            Total number of nodes converted.
+
+        Raises
+        ------
+        ValueError
+            Invalid number of tasks (must be >= 1).
+        """
+        member = await zarr_async.open(store=self.n5_store, path=path)
+
+        if n_tasks < 1:
+            raise ValueError(f"Must use at least 1 task, got {n_tasks}")
+
+        tasks = []
+        await self.queue.put(member)
+        tasks.append(self._spawn_worker(overwrite_zarr_json))
+
+        enqueued = 1
+        if isinstance(member, AsyncGroup) and max_depth != 0:
+            if max_depth < 0:
+                md = None
+            else:
+                md = max_depth - 1
+
+            async for _, child in member.members(md):
+                await self.queue.put(child)
+                if len(tasks) < n_tasks:
+                    tasks.append(self._spawn_worker(overwrite_zarr_json))
+
+                enqueued += 1
+
+        logger.info("Enqueued %s nodes", enqueued)
 
         await self.queue.put(Finished())
         count = sum(await asyncio.gather(*tasks))
         logger.info("Converted %s nodes", count)
         return count
 
-    def _spawn_worker(self, name: str | None = None):
+    def _spawn_worker(self, overwrite_zarr_json: bool, name: str | None = None):
         """Schedule a task for execution wrapping a worker function."""
-        return asyncio.create_task(self._worker(), name=name)
+        return asyncio.create_task(self._worker(overwrite_zarr_json), name=name)
 
-    async def _worker(self):
+    async def _worker(self, overwrite_zarr_json: bool):
         """Create a worker which reads Zarr nodes from the queue and processes them."""
         total = 0
         while True:
@@ -70,19 +111,22 @@ class N5ToZarr:
             if isinstance(value, Finished):
                 await self.queue.put(value)
                 return total
-            total += await self.convert_member(value)
+            total += await self.convert_member(value, overwrite_zarr_json)
             self.queue.task_done()
 
-    async def convert_member(self, member: AsyncArray | AsyncGroup) -> int:
+    async def convert_member(
+        self, member: AsyncArray | AsyncGroup, overwrite_zarr_json: bool = False
+    ) -> int:
         """Returns 0 or 1 for whether the node was skipped or converted."""
-        try:
-            _ = await zarr_async.open(
-                store=self.inner_store, mode="r", path=member.path
-            )
-            logger.info("Found existing zarr node at %s, ignoring", member.path)
-            return 0
-        except Exception:
-            pass
+        if not overwrite_zarr_json:
+            try:
+                _ = await zarr_async.open(
+                    store=self.inner_store, mode="r", path=member.path
+                )
+                logger.info("Found existing zarr node at %s, ignoring", member.path)
+                return 0
+            except Exception:
+                pass
 
         await save_metadata(
             StorePath(store=self.inner_store, path=member.path), member.metadata, False
@@ -95,9 +139,15 @@ async def convert_hierarchy(
     store: StoreLike,
     path: str = "",
     infer_groups: bool = True,
-    max_depth=None,
-    n_tasks=DEFAULT_TASKS,
+    max_depth: int = -1,
+    n_tasks: int = DEFAULT_TASKS,
+    overwrite_zarr_json: bool = False,
 ) -> int:
     inner_store = await make_store(store, mode="r+")
     converter = N5ToZarr(inner_store, infer_groups)
-    return await converter.convert_hierarchy(path, max_depth=max_depth, n_tasks=n_tasks)
+    return await converter.convert_hierarchy(
+        path,
+        max_depth=max_depth,
+        n_tasks=n_tasks,
+        overwrite_zarr_json=overwrite_zarr_json,
+    )
